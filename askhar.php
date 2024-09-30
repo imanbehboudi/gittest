efe4814aadbb918ebe04358968aec89917924c8e
kevin <kevin@DESKTOP-0OIBVUG>
2024-09-30 11:47:26 +0530

    change askhar

--- askhar.php
+++ askhar.php
@@ -5,13 +5,14 @@ $servername = "database-no-2.mysql.database.azure.com"; // e.g., "localhost" or 
 $username = "dbadmin2";
 $password = "834765jhfsd@#$%resad234wqeftEWR";
 $dbname = "sec_wordpress";
-
+echo "sup";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname, 3306);
 
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
+    echo "Not Connected successfully";
 } else {
     echo "Connected successfully";
 }
