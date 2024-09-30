d6743cd94dad97b6bad298bfd127ebbf7a02b5fc
kevin <kevin@DESKTOP-0OIBVUG>
2024-09-30 11:42:39 +0530

    change askhar

--- askhar.php
+++ askhar.php
@@ -1,3 +1,4 @@
+hi
 <?php
 // Database connection details
 $servername = "database-no-2.mysql.database.azure.com"; // e.g., "localhost" or the server IP
@@ -6,7 +7,7 @@ $password = "834765jhfsd@#$%resad234wqeftEWR";
 $dbname = "sec_wordpress";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname, 3306);
 
 // Check connection
 if ($conn->connect_error) {
