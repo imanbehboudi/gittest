4c01b05d3487ee61786ca9f827f4d3bfd838a21e
kevin <kevin@DESKTOP-0OIBVUG>
2024-10-01 10:33:50 +0530

    change askhar

--- askhar2.php
+++ askhar2.php
@@ -3,10 +3,10 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-echo "1";
+echo "2";
 // Database connection details
 $host = "database-no-2.mysql.database.azure.com";
-$username = "dbadmin2@database-no-2";
+$username = "dbadmin2";
 $password = "834765jhfsd@#$%resad234wqeftEWR";
 $database = "sec_wordpress";
 $port = 3306;
